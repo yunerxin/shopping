478488268969e5a40cd0ec5a0b39563395958d13
yuner1114 <2272563803@qq.com>
2020-07-21 18:01:44 +0800

    本地存储

--- src/pages/goods/components/history.vue
+++ src/pages/goods/components/history.vue
@@ -9,36 +9,57 @@
       shape="round"
     />
     <div style="padding:0 .2rem">
-      <p class="title" style="display: flex;justify-content: space-between;align-items: center;font-size: .28rem;"><span>搜索历史</span><van-icon name="delete" size='20px'/></p>
-      <div class="list" v-if="arrayList.lenth>0">
-        <p style="padding-bottom: .2rem;border-bottom: 1px solid #f5f5f5;" v-for="(item,i) in arrayList" :key="i">111</p>
+      <p
+        class="title"
+        style="display: flex;justify-content: space-between;align-items: center;font-size: .28rem;"
+      >
+        <span>搜索历史</span><van-icon name="delete" size="20px" @click="deletHistory"/>
+      </p>
+      <div class="list" v-if="arrayList.length > 0">
+        <p
+          style="padding-bottom: .2rem;border-bottom: 1px solid #f5f5f5;"
+          v-for="(item, i) in arrayList"
+          :key="i"
+        >
+          {{item.value}}
+        </p>
       </div>
-      <p v-else>暂无搜索历史</p>
+      <p v-else style="font-size: .3rem;text-align: center;font-weight: 600;">暂无搜索历史</p>
     </div>
   </div>
 </template>
 <script>
-import { Search, Icon } from "vant";
-import _ from "lodash"
+import { Search, Icon,} from "vant";
+import _ from "lodash";
 export default {
   components: {
     [Search.name]: Search,
-    [Icon.name]: Icon
+    [Icon.name]: Icon,
   },
   data() {
     return {
       searchVal: "",
-      arrayList:[]
+      arrayList: []
     };
   },
-  created(){
-    this.arrayList = JSON.parse(localStorage.getItem('historyArray')) || [];
+  created() {
+    this.arrayList = JSON.parse(localStorage.getItem("historyArray") || "[]");
+    console.log('this.arrayList',this.arrayList.length)
   },
   methods: {
     onSearch(value) {
-      if(value.replace(/\s*/g,"")){
-        localStorage.setItem('historyArray',this.arrayList.push(value))
+      if (value.replace(/\s*/g, "")) {
+        let obj = {
+          value: value
+        };
+        this.arrayList.push(obj)
+        this.arrayList = _.uniqWith(this.arrayList,_.isEqual);
+        localStorage.setItem("historyArray", JSON.stringify(this.arrayList));
       }
+    },
+    deletHistory(){
+      this.arrayList = []
+      localStorage.setItem("historyArray", JSON.stringify('[]'));
     },
     onCancel() {
       // console.log()
@@ -46,6 +67,4 @@ export default {
   }
 };
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/pages/goods/components/searchList.vue
+++ src/pages/goods/components/searchList.vue
@@ -1,10 +1,30 @@
 <template>
-    <div></div>
+    <div>
+        <van-nav-bar
+  left-arrow
+  @click-left="onClickLeft"
+  @click-right="onClickRight"
+>
+<van-search v-model="searchValue" placeholder="请输入搜索关键词" slot="title"/>
+<van-icon :name="icon" slot="right"/>
+</van-nav-bar>
+    </div>
 </template>
 <script>
+import { Search, Icon, NavBar } from "vant";
+import cardIcon from "@/assets/search/card@2x.png"
+import listIcon from "@/assets/search/list@2x.png"
 export default {
+    components:{
+        [Search.name]:Search,
+        [Icon.name]:Icon,
+        [NavBar.name]:NavBar
+    },
     data(){
-        return {}
+        return {
+            searchValue:'',
+            icon:cardIcon,
+        }
     }
 }
 </script>
